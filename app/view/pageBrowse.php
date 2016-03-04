34b2568eee63f9a71f6b26b6feb8cf56be126503
William <wfm5@njit.edu>
2016-03-04 13:40:44 -0500

    uncaught error: call to member function prepare() on null

--- app/view/pageBrowse.php
+++ app/view/pageBrowse.php
@@ -2,8 +2,8 @@
 namespace app\view;
 use app\model as model;
 
-class pageBrowse extends model\pageTemplate{
-	
+class pageBrowse extends model\pageTemplate
+{
 	//page that shows book data based on
 
 	private $db;
@@ -14,9 +14,40 @@ class pageBrowse extends model\pageTemplate{
 
 	}
 
-	public function getBody(){
-		
-	}
+
+	public function mysql_query_or_die($query) 
+		{
+	    	$result = mysql_query($query);
+		    if ($result)
+		        return $result;
+		    else 
+		    {
+		        $err = mysql_error();
+		        die("<br>{$query}<br>*** {$err} ***<br>");
+	    	}
+	    }
+		public function getBody()
+		{
+			try{
+				$rowcount;
+
+				$stmt = $this->db->prepare('select * from inventory where item_ID <= 3');
+
+				if($stmt->execute())
+				{
+					while($data = $stmt->fetch())
+					{
+						$rowcount = $rowcount+1;
+						echo 'Book1:'.$data[0].'';
+					}
+				}
+
+
+			}catch(Exception $e)
+			{
+				echo'errrrrrrrrrrrror';
+			}
+		}
 
 } 
 
